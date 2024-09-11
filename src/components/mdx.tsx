48714531b42cbeacbb7146f1ec5f4bfacbb14887
girish.chaudhari <girish.chaudhari@visaero.com>
2024-09-11 20:25:24 +0530

    resolved changes

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,6 +1,11 @@
+// "use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
+import { LiveCode } from "./sandpack";
+import { TweetComponent } from "./tweet";
+import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote/rsc";
 
 function Table({ data }: { data: { headers: string[]; rows: string[][] } }) {
   let headers = data.headers.map((header, index) => (
@@ -127,20 +132,20 @@ interface ToggleProps {
   initialOn?: boolean;
 }
 
-const Toggle: React.FC<ToggleProps> = ({ initialOn = false }) => {
-  const [isOn, setIsOn] = useState(initialOn);
+// const Toggle: React.FC<ToggleProps> = ({ initialOn = false }) => {
+//   const [isOn, setIsOn] = useState(initialOn);
 
-  return (
-    <button
-      onClick={() => setIsOn(!isOn)}
-      className={`px-4 py-2 rounded-md ${
-        isOn ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
-      }`}
-    >
-      {isOn ? "On" : "Off"}
-    </button>
-  );
-};
+//   return (
+//     <button
+//       onClick={() => setIsOn(!isOn)}
+//       className={`px-4 py-2 rounded-md ${
+//         isOn ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
+//       }`}
+//     >
+//       {isOn ? "On" : "Off"}
+//     </button>
+//   );
+// };
 
 interface TodoProps {
   text: string;
@@ -282,6 +287,83 @@ const Reminder: React.FC<ReminderProps> = ({ children }) => {
   );
 };
 
+interface CalloutProps {
+  emoji: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const Callout: React.FC<CalloutProps> = ({ emoji, children }) => {
+  return (
+    <div className="px-4 py-3 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm flex items-center text-neutral-900 dark:text-neutral-100 mb-8">
+      <div className="flex items-center w-4 mr-4">{emoji}</div>
+      <div className="w-full callout">{children}</div>
+    </div>
+  );
+};
+
+interface ProsConsCardProps {
+  title: string;
+  pros: string[];
+}
+
+const ProsCard: React.FC<ProsConsCardProps> = ({ title, pros }) => {
+  return (
+    <div className="border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-4 w-full">
+      <span>{`You might use ${title} if...`}</span>
+      <div className="mt-4">
+        {pros.map((pro) => (
+          <div key={pro} className="flex font-medium items-baseline mb-2">
+            <div className="h-4 w-4 mr-2">
+              <svg className="h-4 w-4 text-emerald-500" viewBox="0 0 24 24">
+                <g
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
+                  <path d="M22 4L12 14.01l-3-3" />
+                </g>
+              </svg>
+            </div>
+            <span>{pro}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+interface ConsCardProps {
+  title: string;
+  cons: string[];
+}
+
+const ConsCard: React.FC<ConsCardProps> = ({ title, cons }) => {
+  return (
+    <div className="border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900 rounded-xl p-6 my-6 w-full">
+      <span>{`You might not use ${title} if...`}</span>
+      <div className="mt-4">
+        {cons.map((con) => (
+          <div key={con} className="flex font-medium items-baseline mb-2">
+            <div className="h-4 w-4 mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-4 w-4 text-red-500"
+              >
+                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+              </svg>
+            </div>
+            <span>{con}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export const globalComponents = {
   h1: createHeading(1),
   h2: createHeading(2),
@@ -291,10 +373,15 @@ export const globalComponents = {
   h6: createHeading(6),
   Image: RoundedImage,
   a: CustomLink,
+  Callout,
+  ProsCard,
+  ConsCard,
   Table,
+  LiveCode,
+  StaticTweet: TweetComponent,
   YouTube: YouTubeEmbed,
   Checklist,
-  Toggle,
+  // Toggle,
   Todo,
   NumberedList,
   BulletedList,
@@ -305,3 +392,24 @@ export const globalComponents = {
   Excalidraw: ExcalidrawEmbed,
   Reminder,
 };
+
+// export function CustomMDX(props) {
+//   return (
+//     <MDXRemote
+//       {...props}
+//       components={{ ...components, ...(props.components || {}) }}
+//     />
+//   );
+// }
+
+interface Props {
+  mdxSource: MDXRemoteSerializeResult;
+}
+
+export default function CustomMDX({mdxSource} : Props) {
+  return (
+    <div>
+      <MDXRemote {...mdxSource} components={{...globalComponents}} />
+    </div>
+  );
+}
